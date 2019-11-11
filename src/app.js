4428b43a52d43c8db704d0216d69227bc384977c
Just-A-Fool <aedanscarn@gmail.com>
2019-11-11 12:33:19 -0700

    Boilerplate v2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('logger');
+const BOILERPLATERouter = require('BOILERPLATERouter');
 
 const app = express();
 
@@ -12,13 +14,23 @@ const morganOption = (NODE_ENV === 'production')
     : 'dev';
 
 app.use(morgan(morganOption));
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
-app.get('/boilerplate', (req, res) => {
-    res.status(200).json({ message: 'boilerplate' });
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error('Unauthorized request.');
+        return res.status(401).json({ message: 'Unauthorized request' });
+    }
+    next();
 });
 
+app.use(BOILERPLATERouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
